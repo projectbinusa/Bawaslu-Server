2d5f4205c4e0b7d32b1a882906a84863f8f87536
fatiyasalsabila994@gmail.com <fatiyasalsabila994@gmail.com>
2023-10-30 14:40:54 +0700

    pos

---
+++ src/test/java/com/Binusa/BawasluServer/BawasluServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Binusa.BawasluServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BawasluServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
