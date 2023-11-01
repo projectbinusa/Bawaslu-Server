51f81cf946e4160ef9443f58af4fe276c952da28
alfnbntng <alfanbintang28@gmail.com>
2023-11-01 13:11:30 +0700

    membuat role admin

--- src/main/java/com/Binusa/BawasluServer/BawasluServerApplication.java
+++ src/main/java/com/Binusa/BawasluServer/BawasluServerApplication.java
@@ -2,12 +2,13 @@ package com.Binusa.BawasluServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 public class BawasluServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BawasluServerApplication.class, args);
+		System.out.println("selesai");
 	}
-
 }
+
