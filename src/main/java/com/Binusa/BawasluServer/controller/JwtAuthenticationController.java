c09430bcfecf2098f0a380ed765b7b87fc313e2b
din-ovita <dindanovitaputeriutami29@gmail.com>
2023-11-28 21:47:24 +0700

    memperbaiki cross origin

--- src/main/java/com/Binusa/BawasluServer/controller/JwtAuthenticationController.java
+++ src/main/java/com/Binusa/BawasluServer/controller/JwtAuthenticationController.java
@@ -17,7 +17,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "")
+@CrossOrigin(origins = "https://api-bawaslu.excellentsistem.com")
 public class JwtAuthenticationController {
 
     @Autowired
