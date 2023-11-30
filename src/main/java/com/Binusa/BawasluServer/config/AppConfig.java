c830f95f111002f5661c07e61c4d2182abb2739b
din-ovita <dindanovitaputeriutami29@gmail.com>
2023-11-30 08:52:48 +0700

    memperbaiki cross origin

--- src/main/java/com/Binusa/BawasluServer/config/AppConfig.java
+++ src/main/java/com/Binusa/BawasluServer/config/AppConfig.java
@@ -11,6 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class AppConfig extends WebMvcConfigurerAdapter {
     @Override
     public void  addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").maxAge(3600);
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*").maxAge(3600);
     }
 }
