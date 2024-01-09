541037a67098944a42a88762ed80d4721b214c46
alfnbntng <alfanbintang28@gmail.com>
2024-01-09 14:47:54 +0700

    membuat query untuk truncate database4

--- src/main/java/com/Binusa/BawasluServer/repository/JenisInformasiRepository.java
+++ src/main/java/com/Binusa/BawasluServer/repository/JenisInformasiRepository.java
@@ -4,7 +4,6 @@ import com.Binusa.BawasluServer.model.JenisInformasi;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
@@ -12,9 +11,9 @@ import javax.transaction.Transactional;
 @Repository
 public interface JenisInformasiRepository extends JpaRepository<JenisInformasi, Long> {
     @Modifying
-    @Query(value = "DELETE FROM JenisInformasi j WHERE j.id = :id")
+    @Query(value = "DELETE FROM JenisInformasi")
     @Transactional
-    void truncateTable(@Param("id") Long id);
+    void truncateTable();
 }
 
 
